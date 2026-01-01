9d36f1db88bafbe1759e878343a057bc7bda2d70
Vishnu Prasad  J <jvishnuprasad10@gmail.com>
2026-01-01 14:11:09 +0530

    updated code

--- calculator.py
+++ calculator.py
@@ -1,27 +1,37 @@
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
-def mul(a,b):
-    return a*b
-def div(a,b):
-    return a/b
-def mod(a,b):
-    return a%b
-while True:
-    a=int(input("enter a number"))
-    b=int(input("enter a number"))
-    c=input("enter operator")
-    if(c=="+"):
-        print(add(a,b))
-    elif(c=="-"):
-        print(sub(a,b))
-    elif(c=="*"):
-        print(mul(a,b))
-    elif(c=="/"):
-        print(div(a,b))
-    elif(c=="%"):
-        print(mod(a,b))
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+def mod(a, b):
+    return a % b
+
+
+def run():
+    a = int(input("enter a number: "))
+    b = int(input("enter another number: "))
+    c = input("enter operator: ")
+
+    if c == "+":
+        print(add(a, b))
+    elif c == "-":
+        print(sub(a, b))
+    elif c == "*":
+        print(mul(a, b))
+    elif c == "/":
+        print(div(a, b))
+    elif c == "%":
+        print(mod(a, b))
     else:
         print("invalid")
-    break
+
+
+if __name__ == "__main__":
+    run()
